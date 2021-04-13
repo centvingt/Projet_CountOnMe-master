a9e94a8603ef1bd0c9f924b3ef6a17dfe04ae632
Vincent Caronnet <120@120.design>
2021-04-13 02:29:47 +0200

    Déplacement de toutes les données et leur logique dans le modèle Expression

--- CountOnMe/Model/Expression.swift
+++ CountOnMe/Model/Expression.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 
-struct Expression {
-    var elements: [String]
+class Expression {
+    var elements = [String]() {
+        didSet {
+            let notification = Notification(name: .udpatedExpression)
+            NotificationCenter.default.post(notification)
+        }
+    }
     
     // Error check computed variables
     var isCorrect: Bool {
@@ -27,4 +32,94 @@ struct Expression {
     var haveResult: Bool {
         return elements.firstIndex(of: "=") != nil
     }
+    
+    enum ExpressionError {
+        case connection
+        case undefined
+        case response
+        case statusCode
+        case data
+        case pictures
+    }
+    enum Element {
+        case number(String),
+             plus,
+             minus,
+             equal
+    }
+    
+    func add(element: Element) {
+        switch element {
+        case .number(let string):
+            if haveResult {
+                elements = []
+            }
+            elements.append(string)
+        case .plus, .minus:
+            guard canAddOperator else {
+                let notification = Notification(name: .operatorAlreadySet)
+                NotificationCenter.default.post(notification)
+                return
+            }
+            let op = getOperatorString(from: element)
+            elements.append(op)
+        case .equal:
+            setResult()
+        }
+    }
+    private func setResult() {
+        print("plus")
+        guard isCorrect else {
+            let notification = Notification(name: .notCorrectExpression)
+            NotificationCenter.default.post(notification)
+            return
+        }
+        
+        guard haveEnoughElement else {
+            let notification = Notification(name: .notEnoughElement)
+            NotificationCenter.default.post(notification)
+            return
+        }
+        
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        
+        let array = ["=", "\(operationsToReduce.first!)"]
+//        elements.append("= \(operationsToReduce.first!)")
+        elements = elements + array
+    }
+    private func getOperatorString(from element: Element) -> String {
+        switch element {
+        case .plus:
+            return "+"
+        case .minus:
+            return "-"
+        default:
+            fatalError("This is not an operator!")
+        }
+    }
+}
+
+extension Notification.Name {
+    static let udpatedExpression = Notification.Name("udpatedExpression")
+    static let operatorAlreadySet = Notification.Name("operatorAlreadySet")
+    static let notCorrectExpression = Notification.Name("notCorrectExpression")
+    static let notEnoughElement = Notification.Name("notEnoughElement")
 }
